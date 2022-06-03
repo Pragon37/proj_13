68a849919b81ee92a8828879008e0498c41058f0
Pierre Ragon <pragon37@outlook.fr>
2022-06-03 17:26:45 +0200

    Clean up.Lint.Remove unnecessary files.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,12 +4,12 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
     #    path('lettings/', views.lettings_index, name='lettings_index'),
     #    path('lettings/<int:letting_id>/', views.letting, name='letting'),
     #    path('profiles/', views.profiles_index, name='profiles_index'),
     #    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
-    path('', include('lettings.urls')),
-    path('', include('profiles.urls')),
+    path("", include("lettings.urls")),
+    path("", include("profiles.urls")),
+    path("admin/", admin.site.urls),
 ]
